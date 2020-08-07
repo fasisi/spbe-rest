5a1ebe3cb630adcdd1980f3d516a1d0894d176a4
rezachris20 <rezachrismardianto20@gmail.com>
2020-08-07 15:59:30 +0700

    tes api

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=spbe',
+    'dsn' => 'mysql:host=localhost;port=3307;dbname=spbe',
     'username' => 'root',
-    'password' => 'jangandibuka',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
