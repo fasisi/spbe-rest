caf9f04626e0f69e035d07867e94d68a346f3c1a
Frans Indroyono <fasisi2003@yahoo.com>
2020-08-08 18:51:05 +0700

    Menambahkan file /config/restconf.php

---
+++ config/restconf.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+  "class" => 'app\helpers\restconf',
+  "confs" => 
+  [
+    "confluence" => 
+    [
+      "ip" => "202.46.6.3",
+      "port" => "8090",
+      "user" => "ujicoba",
+      "password" => "!UjiCobaSPB3!",
+    ],
+    "jira" => 
+    [
+      "ip" => "202.46.6.2",
+      "port" => "8090",
+      "user" => "ujicoba",
+      "password" => "!UjiCobaSPB3!",
+    ]
+  ]
+];
+
+?>
