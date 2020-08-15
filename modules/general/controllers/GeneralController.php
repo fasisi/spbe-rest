fac300004adb8cd89c7a6ebaa526bcd2aba54953
Frans Indroyono <fasisi2003@yahoo.com>
2020-08-15 20:01:27 +0700

    Menambah logic gettags

--- modules/general/controllers/GeneralController.php
+++ modules/general/controllers/GeneralController.php
@@ -2,11 +2,43 @@
 
 namespace app\modules\general\controllers;
 
-class GeneralController extends \yii\web\Controller
+use app\models\KmsTags;
+
+class GeneralController extends \yii\rest\Controller
 {
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
+  public function behaviors()
+  {
+    date_default_timezone_set("Asia/Jakarta");
+    $behaviors = parent::behaviors();
+    $behaviors['verbs'] = [
+      'class' => \yii\filters\VerbFilter::className(),
+      'actions' => [
+        'gettags'    => ['GET'],
+      ]
+    ];
+    return $behaviors;
+  }
+
+  public function actionIndex()
+  {
+      return $this->render('index');
+  }
+
+  /*
+   *  Mengembalikan daftar tags yang terdaftar dalam database.
+    * */
+  public function actionGettags()
+  {
+    $list = KmsTags::find()
+      ->where(
+        "status = 1"
+      )
+      ->orderBy("name asc")
+      ->all();
+
+    return [
+      "result" => $list
+    ];
+  }
 
 }
