a73eba5e93665a9b0d3e2cbc632cbb7f10aacbd6
Frans Indroyono <fasisi2003@yahoo.com>
2020-08-19 15:52:25 +0700

    Menyiapkan fungsi untuk manage kategori

--- modules/general/controllers/GeneralController.php
+++ modules/general/controllers/GeneralController.php
@@ -14,6 +14,7 @@ class GeneralController extends \yii\rest\Controller
       'class' => \yii\filters\VerbFilter::className(),
       'actions' => [
         'gettags'    => ['GET'],
+        'kategori'   => ['PORT, GET, PUT, DELETE'],
       ]
     ];
     return $behaviors;
@@ -39,6 +40,78 @@ class GeneralController extends \yii\rest\Controller
     return [
       "result" => $list
     ];
+  }
+
+  /*
+   *  Fungsi untuk melakukan management kategori
+   *
+   *  --===== Create Kategori =====----
+   *  Method: POST
+   *  Request type: JSON
+   *  Request format:
+   *  {
+   *    "nama": "abc",
+   *    "id_parent": "-1/123",
+   *    "deskripsi": "abc"
+   *  }
+   *  Response type: JSON
+   *  Response format:
+   *  {
+   *    "status": "ok/not ok",
+   *    "pesan": "",
+   *    "result": { <object_record_kategori> }
+   *  }
+   *
+   *  --===== Get Kategori =====----
+   *  Method: GET
+   *  Request type: JSON
+   *  Request format:
+   *  {
+   *    "id": 123,
+   *  }
+   *  Response type: JSON
+   *  Response format:
+   *  {
+   *    "status": "ok/not ok",
+   *    "pesan": "",
+   *    "result": { <object_record_kategori> }
+   *  }
+   *
+   *  --===== Update Kategori =====----
+   *  Method: PUT
+   *  Request type: JSON
+   *  Request format:
+   *  {
+   *    "id": 123,
+   *    "id_parent": 123,
+   *    "nama": "asdfasdf",
+   *    "deskripsi": "asdfasd"
+   *  }
+   *  Response type: JSON
+   *  Response format:
+   *  {
+   *    "status": "ok/not ok",
+   *    "pesan": "",
+   *    "result": { <object_record_kategori> }
+   *  }
+   *
+   *  --===== DELETE Kategori =====----
+   *  Method: DELETE
+   *  Request type: JSON
+   *  Request format:
+   *  {
+   *    "id": 123,
+   *  }
+   *  Response type: JSON
+   *  Response format:
+   *  {
+   *    "status": "ok/not ok",
+   *    "pesan": "",
+   *    "result": { <object_record_kategori> }
+   *  }
+    * */
+  public function actionKategori()
+  {
   }
 
 }
