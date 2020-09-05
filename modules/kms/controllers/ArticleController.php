53b0724e7d7fcd7ab30d3b154efbabb40bd78eee
rezachris20 <rezachrismardianto20@gmail.com>
2020-09-05 17:54:38 +0700

    no message

--- modules/kms/controllers/ArticleController.php
+++ modules/kms/controllers/ArticleController.php
@@ -208,7 +208,7 @@ class ArticleController extends \yii\rest\Controller
 
     if( $judul_valid == true && $body_valid == true &&
         $kategori_valid == true && $tags_valid == true &&
-	$status_valid = true
+	$status_valid == true
       )
     {
       // panggil POST /rest/api/content
