799f6a89b29ad9a54d269fd9adea081c392aaf59
rezachris20 <rezachrismardianto20@gmail.com>
2020-09-14 11:50:37 +0700

    membuat hak akses user kategori

--- controllers/GlobalController.php
+++ controllers/GlobalController.php
@@ -51,4 +51,90 @@ class GlobalController extends \yii\rest\Controller
             ];
         }
     }
+
+    public function actionHakakseskategori()
+    {
+        $payload = Yii::$app->request->rawBody;
+        Yii::info("payload = $payload");
+        $payload = Json::decode($payload);
+
+        if (isset($payload["id_user"]) == true) {
+            $query = new Query;
+            $query->select('id')
+                ->from("kms_kategori")
+                ->where('id not in (select id_kategori from kategori_user where id_user ='.$payload['id_user'].')');
+
+
+            $command = $query->createCommand();
+            $data = $command->queryAll();
+
+            $query1 = new Query;
+            $query1->select('id')
+                ->from("kms_kategori")
+                ->where('id in (select id_kategori from kategori_user where id_user ='.$payload['id_user'].')')
+                ->limit(1);
+
+
+            $command1 = $query1->createCommand();
+            $data1 = $command1->queryAll();
+
+            if (!empty($data)) {
+                return [
+                    "status" => "ok",
+                    "pesan" => "Record found",
+                    "result" => [
+                        "all_kategori" => $data,
+                        "hak_kategori" => $data1
+                    ]
+                ];
+            } else {
+                return [
+                    "status" => "not ok",
+                    "pesan" => "Record not found",
+                ];
+            }
+        } else {
+            return [
+                "status" => "not ok",
+                "pesan" => "Required parameter: id",
+            ];
+        }
+    }
+
+    public function actionDropdownkategoriwithhakakses()
+    {
+        $payload = Yii::$app->request->rawBody;
+        Yii::info("payload = $payload");
+        $payload = Json::decode($payload);
+
+        if (isset($payload["id_user"]) == true) {
+
+            $query = new Query;
+            $query->select('kms_kategori.*')
+            ->from("kms_kategori")
+            ->where('id in (select id_kategori from kategori_user where id_user ='.$payload['id_user'].')')
+            ->orderBy('nama asc');
+
+            $command = $query->createCommand();
+            $data = $command->queryAll();
+
+            if (!empty($data)) {
+                return [
+                    "status" => "ok",
+                    "pesan" => "Record found",
+                    "result" => $data
+                ];
+            } else {
+                return [
+                    "status" => "not ok",
+                    "pesan" => "Record not found",
+                ];
+            }
+        } else {
+            return [
+                "status" => "not ok",
+                "pesan" => "Required parameter: id",
+            ];
+        }
+    }
 }
